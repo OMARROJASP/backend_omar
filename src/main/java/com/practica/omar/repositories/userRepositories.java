fde2f8a237f2b98214566457a71a278cf10cbe9e
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2024-01-18 19:30:40 -0500

    login Jpa completo

--- src/main/java/com/practica/omar/repositories/userRepositories.java
+++ src/main/java/com/practica/omar/repositories/userRepositories.java
@@ -1,11 +1,18 @@
 package com.practica.omar.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.practica.omar.model.entities.User;
 
+import java.util.Optional;
+
 public interface userRepositories extends CrudRepository<User, Long> {
-    
+
+    Optional<User> findByUsername(String username);
+
+    @Query("select u from User u where u.username=?1")
+    Optional<User> getUserByUsername(String username);
 
 
 }
--- src/main/java/com/practica/omar/service/JpaUserDetailsService.java
+++ src/main/java/com/practica/omar/service/JpaUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.practica.omar.service;
 
+import com.practica.omar.repositories.userRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,27 +9,38 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private userRepositories repositories;
+
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("admin")) {
+        Optional<com.practica.omar.model.entities.User> o = repositories.findByUsername(username);
+
+
+        if (!o.isPresent()) {
             throw new UsernameNotFoundException(String.format("Username %s no existe en el sistema!", username));
         }
-
+        com.practica.omar.model.entities.User user = o.orElseThrow();
         //Si es igual lo authenticamos
         List<GrantedAuthority> authorities = new ArrayList<>();
         
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
         // debemos retornar el user security, userdetails
         // el primer true habilitado, no expira, credenciales, que no esta bloqueado, roles
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
                 true,
                 true,
                 true,
